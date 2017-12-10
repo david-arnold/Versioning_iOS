7f1a419b127ec72102b25a4795274fdd28158409
David Arnold <darnold@blackberry.com>
2017-12-10 15:04:12 +0000

    Initial Commit

---
+++ Versioning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Versioning
+//
+//  Created by David Arnold on 10/12/2017.
+//  Copyright © 2017 David Arnold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Versioning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Versioning
+//
+//  Created by David Arnold on 10/12/2017.
+//  Copyright © 2017 David Arnold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
